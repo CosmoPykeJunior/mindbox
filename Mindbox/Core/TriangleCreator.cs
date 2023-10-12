89283c09306d618b172e96810137fe3acb2d8622
Попов Евгений Артёмович <popov_evgeniy94@bk.ru>
2023-10-12 21:53:14 +0300

    add factory + xunit instead of nunit

---
+++ Mindbox/Core/TriangleCreator.cs
@@ -0,0 +1,21 @@
+﻿using Mindbox.Interfaces;
+
+namespace Mindbox.Core
+{
+    public interface IShapeFactory
+    {
+
+        public IShape GetShape(double a, double b, double c);
+        public IShape GetShape(double radius);
+    }
+
+    public class ShapeFactory : IShapeFactory
+    {
+        public IShape GetShape(double a, double b, double c) =>
+            new Triangle.Triangle(a, b, c);
+
+        public IShape GetShape(double radius) =>
+            new Circle.Circle(radius);
+    }
+}
+
--- ShapeTests/ShapeTests.cs
+++ ShapeTests/ShapeTests.cs
@@ -1,78 +1,119 @@
 using Mindbox.Circle;
 using Mindbox.Core;
+using Mindbox.Interfaces;
 using Mindbox.Triangle;
+using Moq;
 
 namespace ShapeTests
 {
-
-
     public class Tests
     {
-
         private static readonly int DIGITS_COUNT_AFTER_COMMA = 2;
 
-        [Test]
+        [Fact]
         public void CircleInvalidTest() => Assert.Throws<ArgumentOutOfRangeException>(() => new Circle(-1));
 
-        [Test]
+        [Fact]
         public void CircleSquareTest()
         {
-            Circle circle = new(10);
+            var creator = InitShapeFactoryMoq(radius:10).Object;
+
+            IShape circle = creator.GetShape(10);
+
             double expectedSquare = 314.16;
 
-            Assert.That(expectedSquare, Is.EqualTo(ShapeProcessor.CalculateSquare(circle, DIGITS_COUNT_AFTER_COMMA)));
+            Assert.Equal(expectedSquare, ShapeProcessor.CalculateSquare(circle, DIGITS_COUNT_AFTER_COMMA));
         }
 
-        [Test]
+        [Fact]
         public void TriangleInvalideSidesTest()
         {
             //invalid sides
-            Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(-2, 0, 0));
-            Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(0, -2, 0));
-            Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(0, 0, -2));
-            Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(-2, -2, -2));
+            Assert.Throws<ArgumentOutOfRangeException>(() => InitShapeFactoryMoq(new double[] { -2, 0, 0 }));
+            Assert.Throws<ArgumentOutOfRangeException>(() => InitShapeFactoryMoq(new double[] { 0, -2, 0 }));
+            Assert.Throws<ArgumentOutOfRangeException>(() => InitShapeFactoryMoq(new double[] { 0, 0, -2 }));
+            Assert.Throws<ArgumentOutOfRangeException>(() => InitShapeFactoryMoq(new double[] { -2, -2, -2 }));
 
             //triangle not exists
-            Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(1, 1, 4));
+            Assert.Throws<ArgumentOutOfRangeException>(() => InitShapeFactoryMoq(new double[] { -2, -2, -2 }));
         }
 
-        [Test]
+        [Fact]
         public void TriangleSquareTest()
         {
-            var triangle = new Triangle(1, 2, 2);
+            var creator = InitShapeFactoryMoq(new double[] { 1, 2, 2 }).Object;
+
+            IShape triangle = creator.GetShape(1, 2, 2);
 
             double square = 0.97;
 
-            Assert.That(square, Is.EqualTo(ShapeProcessor.CalculateSquare(triangle, DIGITS_COUNT_AFTER_COMMA)));
+            Assert.Equal(square, ShapeProcessor.CalculateSquare(triangle, DIGITS_COUNT_AFTER_COMMA));
         }
 
-        [Test]
+        [Fact]
         public void RightTriangleTest()
         {
-            var triangle = new Triangle(3, 4, 5);
-            Assert.That(triangle.IsRightAngledTriangle, Is.True);
+            var creator = InitShapeFactoryMoq(new double[] { 3, 4, 5 }).Object;
+
+            if (creator.GetShape(3, 4, 5) is Triangle triangle)
+                Assert.True(triangle.IsRightAngledTriangle);
+            else
+                Assert.Fail();
         }
 
-        [Test]
+        [Fact]
         public void NotRightTriangleTest()
         {
-            var triangle = new Triangle(1, 1, 1);
+            var creator = InitShapeFactoryMoq(new double[] { 1, 1, 1 }).Object;
 
-            Assert.That(triangle.IsRightAngledTriangle, Is.False);
+            if (creator.GetShape(1, 1, 1) is Triangle triangle)
+                Assert.False(triangle.IsRightAngledTriangle);
+            else
+                Assert.Fail();
         }
 
-        [Test]
+        [Fact]
         public void ValidDigitsAfterCommaTest()
         {
-            var triangle = new Triangle(2, 2, 2);
-            double square = 2.00;
-            Assert.That(square, Is.EqualTo(Math.Floor(ShapeProcessor.CalculateSquare(triangle))));
+            var creator = InitShapeFactoryMoq(new double[] { 1, 2, 2 }).Object;
+
+            IShape triangle = creator.GetShape(1, 2, 2);
+
+            double square = 1.00;
+            Assert.Equal(square, Math.Floor(ShapeProcessor.CalculateSquare(triangle)));
         }
 
-        [Test]
-        public void NullableShapeProcessorTest()
-        {
+        [Fact]
+        public void NullableShapeProcessorTest() =>
             Assert.Throws<ArgumentNullException>(() => ShapeProcessor.CalculateSquare(default));
+
+        [Fact]
+        public void CreatorReturnValidTypeOfShapeTest()
+        {
+            var creator = InitShapeFactoryMoq(new double[] { 1, 1, 2 }, 1).Object;
+
+            IShape triangle = creator.GetShape(1, 1, 2);
+            IShape circle = creator.GetShape(1);
+
+            Assert.True(triangle is Triangle);
+            Assert.False(triangle is Circle);
+            Assert.False(circle is Triangle);
+            Assert.True(circle is Circle);
         }
+
+
+        private Mock<IShapeFactory> InitShapeFactoryMoq(double[] sides = null, double? radius = null)
+        {
+            var mockFactory = new Mock<IShapeFactory>();
+
+            if(radius!= null)
+            mockFactory.Setup(x => x.GetShape(radius.Value)).Returns(new Circle(radius.Value));
+
+            if(sides!= null)
+            mockFactory.Setup(x => x.GetShape(sides[0], sides[1], sides[2])).Returns(new Triangle(sides[0], sides[1], sides[2]));
+
+            return mockFactory;
+        }
+
     }
 }
